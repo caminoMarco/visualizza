9a03a1ac99857a20c3abb3a06243fee4770c596e
caminoMarco <176792@edu.unito.it>
2016-08-12 18:10:26 +0200

    primo commit

---
+++ src/main/java/com/home/grafico/visualizza/Grafico.java
@@ -0,0 +1,38 @@
+package com.home.grafico.visualizza;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+
+public class Grafico {
+	
+	
+     public static void main( String[] args ) 
+     { 
+         System.out.println( "Prova di jfreechart incluso con maven 1" );  
+          
+         DefaultPieDataset objDataset = new DefaultPieDataset(); 
+         objDataset.setValue("Apple",29); 
+         objDataset.setValue("HTC",15); 
+         objDataset.setValue("Samsung",24); 
+         objDataset.setValue("LG",7); 
+         objDataset.setValue("Motorola",10); 
+          
+         JFreeChart objChart = ChartFactory.createPieChart ( 
+         	    "Demo Pie Chart",   //Chart title 
+         	    objDataset,         //Chart Data  
+         	    true,               // include legend? 
+         	    true,               // include tooltips? 
+         	    false               // include URLs? 
+         	    ); 
+          
+         ChartFrame frame = new ChartFrame("Demo", objChart); 
+         frame.pack(); 
+         frame.setVisible(true); 
+         System.out.println( "Prova di jfreechart incluso con maven 2" ); 
+ 
+     } 
+
+
+}
