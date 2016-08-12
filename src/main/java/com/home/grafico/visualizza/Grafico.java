1b4ffb4eaafa5fdce31dad42ebac1f78e036c690
caminoMarco <176792@edu.unito.it>
2016-08-12 21:57:32 +0200

    commit master 3

--- src/main/java/com/home/grafico/visualizza/Grafico.java
+++ src/main/java/com/home/grafico/visualizza/Grafico.java
@@ -33,6 +33,7 @@ public class Grafico {
          System.out.println( "Prova di jfreechart incluso con maven 2" ); 
          System.out.println( "master 2.1" );
          System.out.println( "master 2.2" );
+         System.out.println( "master 2.3" );
  
      } 
 
